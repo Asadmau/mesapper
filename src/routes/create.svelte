ca4eda224fc0344f7e5926709f0eea0984db8715
github_asadmau <asadulkon@gmail.com>
2020-05-04 03:11:15 +0700

    upgrate

--- src/routes/create.svelte
+++ src/routes/create.svelte
@@ -1,1 +1,9 @@
 <h1>create</h1>
+<h2>Alhamdulillah success!</h2>
+
+<figure>
+	<img alt='Success Kid' src='successkid.jpg'>
+	<figcaption>Have fun with Sapper!</figcaption>
+</figure>
+
+<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
